a6fd8b56bf797c9571ed623d5f2c5525bb2cf5ed
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-07 09:58:34 -0500

    refactor to a class and add runner file

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,36 +1,49 @@
-# Iterate through the numbers 1 to 1000
-# For each one, print the output according to the rules of Super Fizz Buzz
+class SuperFizzBuzz
 
-def super_fizz_buzz(number)
-  if divisible_by_3?(number) && divisible_by_5?(number) && divisible_by_7?(number)
-    'SuperFizzBuzz'
-  elsif divisible_by_3?(number) && divisible_by_7?(number)
-    'SuperFizz'
-  elsif divisible_by_5?(number) && divisible_by_7?(number)
-    'SuperBuzz'
-  elsif divisible_by_3?(number) && divisible_by_5?(number)
-    'FizzBuzz'
-  elsif divisible_by_3?(number)
-    'Fizz'
-  elsif divisible_by_5?(number)
-    'Buzz'
-  elsif divisible_by_7?(number)
-    'Super'
-  else
-    number
+  def initialize
+  end
+
+  def calculate(number)
+    if divisible_by_3?(number) && divisible_by_5?(number) && divisible_by_7?(number)
+      'SuperFizzBuzz'
+    elsif divisible_by_3?(number) && divisible_by_7?(number)
+      'SuperFizz'
+    elsif divisible_by_5?(number) && divisible_by_7?(number)
+      'SuperBuzz'
+    elsif divisible_by_3?(number) && divisible_by_5?(number)
+      'FizzBuzz'
+    elsif divisible_by_3?(number)
+      'Fizz'
+    elsif divisible_by_5?(number)
+      'Buzz'
+    elsif divisible_by_7?(number)
+      'Super'
+    else
+      number.to_s
+    end
+  end
+
+  def output(number)
+    calculate(number)
+  end
+
+  def output_range(number_1, number_2)
+    results = []
+    (number_1..number_2).each { |number| results << calculate(number) }
+    results
+  end
+
+  def divisible_by_3?(number)
+    number % 3 == 0
+  end
+
+  def divisible_by_5?(number)
+    number % 5 == 0
+  end
+
+  def divisible_by_7?(number)
+    number % 7 == 0
   end
 end
 
-def divisible_by_3?(number)
-  number % 3 == 0
-end
-
-def divisible_by_5?(number)
-  number % 5 == 0
-end
-
-def divisible_by_7?(number)
-  number % 7 == 0
-end
-
-(1..1000).each {|number| puts super_fizz_buzz(number)}
+#(1..1000).each {|number| puts super_fizz_buzz(number)}
---
+++ lib/super_fizz_buzz_runner.rb
@@ -0,0 +1,4 @@
+require_relative './super_fizz_buzz'
+super_fizz = SuperFizzBuzz.new
+
+require 'pry'; binding.pry
